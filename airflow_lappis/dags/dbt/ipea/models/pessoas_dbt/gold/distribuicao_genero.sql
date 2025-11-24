48c55b0a6259e55c2309697ad016f824f91151ed
davi-aguiar-vieira <davideaguiarvieira@gmail.com>
2025-11-23 23:05:15 -0300

    refactor: refatora modelos dbt

--- airflow_lappis/dags/dbt/ipea/models/pessoas_dbt/gold/distribuicao_genero.sql
+++ airflow_lappis/dags/dbt/ipea/models/pessoas_dbt/gold/distribuicao_genero.sql
@@ -1,50 +1,8 @@
-with
-    hierarquia_enriquecida as (
-        select
-            ph.*,
-            case
-                when df.modalidade_pgd is null
-                then 'Não participa'
-                when df.modalidade_pgd = 'parcial'
-                then 'Parcial'
-                when df.modalidade_pgd = 'integral'
-                then 'Integral'
-                when df.modalidade_pgd = 'presencial'
-                then 'Presencial'
-                when df.modalidade_pgd = 'no exterior'
-                then 'No exterior'
-            end as pdg,
-            case
-                when ph.nome_situacao_funcional = 'ATIVO EM OUTRO ORGAO'
-                then 'Ativo em outro órgão'
-                else siglaunidade
-            end as unidade_exercicio
-        from {{ ref("hierarquia") }} ph
-        inner join {{ ref("dados_funcionais") }} df on ph.cpf = df.cpf
-    ),
-
-    servidores_enriquecidos as (
-        select distinct ph.*, du.nome_municipio_uorg
-        from hierarquia_enriquecida ph
-        inner join {{ ref("dados_uorg") }} du on ph.siglaunidade = du.sigla_uorg
-        order by caminho_unidade, hierarquia_cargo
-    ),
-
-    servidores_completos as (
-        select distinct
-            se.*,
-            sd.cod_escolaridade_principal,
-            sd.nome_escolaridade_principal,
-            sd.nome_deficiencia_fisica,
-            sd.nome_cargo as nome_cargo_emprego
-        from servidores_enriquecidos se
-        inner join {{ ref("servidores_detalhados") }} sd on se.cpf = sd.cpf
-    )
-
+-- Distribuição de servidores por gênero
 select
     nome_sexo as genero,
     count(*) as quantidade_servidores,
     count(*) * 1.0 / sum(count(*)) over () as percentual_distribuicao
-from servidores_completos
+from {{ ref("servidores_completos") }}
 group by nome_sexo
 order by percentual_distribuicao desc
--- airflow_lappis/dags/dbt/ipea/models/pessoas_dbt/gold/distribuicao_raca_cor.sql
+++ airflow_lappis/dags/dbt/ipea/models/pessoas_dbt/gold/distribuicao_raca_cor.sql
@@ -1,47 +1,5 @@
-with
-    hierarquia_enriquecida as (
-        select
-            ph.*,
-            case
-                when df.modalidade_pgd is null
-                then 'Não participa'
-                when df.modalidade_pgd = 'parcial'
-                then 'Parcial'
-                when df.modalidade_pgd = 'integral'
-                then 'Integral'
-                when df.modalidade_pgd = 'presencial'
-                then 'Presencial'
-                when df.modalidade_pgd = 'no exterior'
-                then 'No exterior'
-            end as pdg,
-            case
-                when ph.nome_situacao_funcional = 'ATIVO EM OUTRO ORGAO'
-                then 'Ativo em outro órgão'
-                else siglaunidade
-            end as unidade_exercicio
-        from {{ ref("hierarquia") }} ph
-        inner join {{ ref("dados_funcionais") }} df on ph.cpf = df.cpf
-    ),
-
-    servidores_enriquecidos as (
-        select distinct ph.*, du.nome_municipio_uorg
-        from hierarquia_enriquecida ph
-        inner join {{ ref("dados_uorg") }} du on ph.siglaunidade = du.sigla_uorg
-        order by caminho_unidade, hierarquia_cargo
-    ),
-
-    servidores_completos as (
-        select distinct
-            se.*,
-            sd.cod_escolaridade_principal,
-            sd.nome_escolaridade_principal,
-            sd.nome_deficiencia_fisica,
-            sd.nome_cargo as nome_cargo_emprego
-        from servidores_enriquecidos se
-        inner join {{ ref("servidores_detalhados") }} sd on se.cpf = sd.cpf
-    )
-
+-- Distribuição de servidores por raça/cor
 select nome_cor as cor_raca, count(nome_cor) as quantidade_servidores
-from servidores_completos
+from {{ ref("servidores_completos") }}
 group by nome_cor
 order by quantidade_servidores desc
