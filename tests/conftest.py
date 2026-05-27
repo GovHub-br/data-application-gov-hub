bba2cf00d57d593f74503e83e83af7752d9b0547
lcsgborges <lcsgborges@gmail.com>
2026-05-27 10:55:41 -0300

    fix(tests): inclui helpers no sys.path do conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,7 @@ sys.path.insert(
     0,
     os.path.abspath(os.path.join(os.path.dirname(__file__), "../airflow_lappis/plugins")),
 )
+sys.path.insert(
+    0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "../airflow_lappis/helpers")),
+)
