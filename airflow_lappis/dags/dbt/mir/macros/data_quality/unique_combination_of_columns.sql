3149a5da9a0196b0ed10205d299d205b8913254c
MatheussBrant <matheus.s.brant@gmail.com>
2026-06-29 12:55:47 -0300

    test(dbt): adiciona testes de integridade em metadata

---
+++ airflow_lappis/dags/dbt/mir/macros/data_quality/unique_combination_of_columns.sql
@@ -0,0 +1,14 @@
+{% macro test_unique_combination_of_columns(model, combination_of_columns) %}
+    with
+        validation_errors as (
+            select
+                {{ combination_of_columns | join(', ') }},
+                count(*) as row_count
+            from {{ model }}
+            group by {{ combination_of_columns | join(', ') }}
+            having count(*) > 1
+        )
+
+    select *
+    from validation_errors
+{% endmacro %}
